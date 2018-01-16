6833a84a58effdefaa3602f10b7274d44c7aa668
Loffew <Loffew@users.noreply.github.com>
2018-01-16 17:32:34 +0800

    ...

--- Project Euler 050 to 100.py
+++ Project Euler 050 to 100.py
@@ -445,7 +445,7 @@ def No_59_XOR_decryption(guess=32):
 
 
 def No_60_Prime_pair_sets():
-    from itertools import combinations
+    from itertools import combinations  # 不重复的组合
     lis = common_for_euler.prime_list(10000)
     ll = []
     for i in combinations(lis, 5):
@@ -960,3 +960,99 @@ def No_68_Magic_5_gon_ring():
                 y = x[:1] + x[5:7] + x[1:2] + x[6:8] + x[2:3] + x[7:9] + x[3:4] + x[8:] + x[4:5] + x[9:] + x[5:6]
                 num = max(num, int("".join([str(k) for k in y])))
     return num
+
+# Project Euler No.69
+'''
+欧拉总计函数与最大值
+
+在小于n的数中，与n互质的数的数目记为欧拉总计函数φ(n)（有时也称为φ函数）。例如，因为1、2、4、5、7和8均小于9且与9互质，故φ(9)=6。
+
+n	互质的数	φ(n)	n/φ(n)
+2	1	1	2
+3	1,2	2	1.5
+4	1,3	2	2
+5	1,2,3,4	4	1.25
+6	1,5	2	3
+7	1,2,3,4,5,6	6	1.1666…
+8	1,3,5,7	4	2
+9	1,2,4,5,7,8	6	1.5
+10	1,3,7,9	4	2.5
+可以看出，对于n ≤ 10，当n=6时n/φ(n)取得最大值。
+
+当n ≤ 1,000,000时，求使得n/φ(n)取得最大值的n。
+
+1、不能为质数
+2、因子越多，结果越大
+结果：100W内，因子(除自己和1外)最多的合数
+
+再分析，如果分解质因子的集合越多，说明欧拉总计函数越小[集合中包含相同的数，即为不互质]
+所以直接从第一个质数开始往上乘[2x3x5x7x9x....]，在规定范围内，组成的最大的数就是 使得n/φ(n)取得最大值的n
+如果在此范围内，数值过小，可以用这个数再从头开始乘以这些质数，试试会不会超过范围
+'''
+def No_69_Totient_maximum(max_range=10**6):
+    num_list = common_for_euler.prime_list(int(max_range**(1/2)))
+    num = 1
+    pri_list = []
+    for i in num_list:
+        num *= i
+        pri_list.append(i)
+        if num > max_range:
+            num /= i
+            pri_list.pop()
+            break
+    # 校验最大数 比如在100以内是90[2,3,5,3]
+    while True:
+        if num * pri_list[-1] > max_range: pri_list.pop()
+        else: num *= pri_list[-1]
+        if len(pri_list) == 0: return int(num)
+'''
+欧拉函数就是指：对于一个正整数n，小于n且和n互质的正整数（包括1）的个数，记作φ(n) 。
+欧拉函数的通式：φ(n)=n*(1-1/p1)*(1-1/p2)*(1-1/p3)*(1-1/p4)…..(1-1/pn),其中p1, p2……pn为n的所有质因数，n是不为0的整数。
+φ(1)=1（唯一和1互质的数就是1本身）。
+用遍历的方法[遍历始终是最慢的]
+y = 0
+num = 0
+for i in range(1,10**6+1):
+    x = i
+    for j in set(common_for_euler.isPrime_factor(i)):
+        x *= 1-1/j
+    if y < i/x:
+        y = i/x
+        num = i
+'''
+
+# Project Euler No.70
+'''
+欧拉总计函数与重排
+
+在小于n的数中，与n互质的数的数目记为欧拉总计函数φ(n)（有时也称为φ函数）。例如，因为1、2、4、5、7和8均小于9且与9互质，故φ(9)=6。
+
+1被认为和任意正整数互质，所以φ(1)=1。
+
+有趣的是，φ(87109)=79180，而79180恰好是87109的一个重排。
+
+在1 < n < 10的7次方中，有些n满足φ(n)是n的一个重排，求这些取值中使n/φ(n)最小的一个。
+
+8319823 跑了好久，终于出来了
+
+用上题的思路，本题的分解质因子的集合越少，最好是n=2个质因子相乘[因为肯定不是质数]
+'''
+def No_70_Totient_permutation(max_range=10**7):
+    lis = common_for_euler.prime_list(int(max_range / 2), 2)  # 要保证2*最大质数 刚好在范围内
+    num = 0
+    cha = 10
+
+    for i in range(len(lis)):
+        for j in range(i + 1, len(lis)):
+            if lis[i] * lis[j] < max_range:
+                x = y = lis[i] * lis[j]
+                x *= 1 - 1 / lis[i]
+                x *= 1 - 1 / lis[j]
+                if sorted(str(y)) == sorted(str(int(x))):
+                    if cha > y / x:
+                        cha = y / x
+                        num = y
+            else:
+                break
+    return num
+
